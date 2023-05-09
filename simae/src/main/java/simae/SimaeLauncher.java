fc52a4168fa2b42ec69888e4300237b0672ade9a
Valentin Fontana <valefontana15@gmail.com>
2023-05-09 18:48:49 -0300

    agregada version inicial de simae para vscode

--- simae/src/main/java/simae/SimaeLauncher.java
+++ simae/src/main/java/simae/SimaeLauncher.java
@@ -2,11 +2,12 @@ package simae;
 
 import simae.lib.Lenguaje;
 import simae.lib.Simae;
-
+import java.util.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import simae.lib.AnotacionMarca;
 
 public class SimaeLauncher {
 
@@ -51,64 +52,10 @@ public class SimaeLauncher {
         return true;
     }
 
-    private boolean writeFile(String outputFileName) {
-        try {
-            inputReader.close();
-            Files.move(Path.of(workFile.getPath()), Path.of(outputFileName), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            System.out.println("Fallo en la escritura del archivo de trabajo");
-            return false;
-        }
-
-        return true;
-    }
-
-    public int launchTagging(File inputFile, String outputFileName, String lenguajeString) {
-
+    public List<AnotacionMarca> obtenerMarcas(File inputFile, String lenguajeString) {
         prepareSimae(inputFile);
-
         Lenguaje lenguaje = lenguaje(lenguajeString.toLowerCase());
-
-
-        try {
-            Simae.fuenteMarcado(inputReader, workWriter, lenguaje, null);
-            workWriter.close();
-        } catch (IOException e) {
-            //System.out.println("Fallo en el proceso de escritura de marcas"); FIXME: este mensaje se deberia dar en la CLI, no en launchTagging
-            return 1;
-        }
-
-        return writeFile(outputFileName) ? 0 : 2;
-    }
-
-    public static String launchTagging(String entrada, Lenguaje lenguaje, String idioma) throws IOException {
-
-        StringReader srEntrada = new StringReader(entrada);
-        BufferedReader reader = new BufferedReader(srEntrada);
-
-        StringWriter swSalida = new StringWriter();
-        PrintWriter writer = new PrintWriter(swSalida);
-
-        Simae.fuenteMarcado(reader, writer, lenguaje, idioma);
-
-        String salida = swSalida.toString();
-
-        srEntrada.close();
-        swSalida.close();
-        return salida;
-    }
-
-    public boolean launchUntagging(File inputFile, String outputFileName, String lenguajeString) {
-
-        prepareSimae(inputFile);
-
-        Lenguaje lenguaje = lenguaje(lenguajeString.toLowerCase());
-
-        Simae.fuenteDesmarcado(inputReader, workWriter, lenguaje);
-        workWriter.close();
-
-        return writeFile(outputFileName);
-
+        return Simae.fuenteMarcado(inputReader, lenguaje, null);
     }
 
 
--- simae/src/main/java/simae/lib/Simae.java
+++ simae/src/main/java/simae/lib/Simae.java
@@ -29,7 +29,6 @@ public class Simae {
 
 		st = new StringTags((language != null) ? language : "");
 		strings = st.getStrings();
-
 		Lexer lexer = ANTLRFactory.getLexer(lenguaje, antlrEntrada);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		Parser parser = ANTLRFactory.getParser(lenguaje, tokens);
@@ -37,123 +36,27 @@ public class Simae {
 		ParseTreeListener extractor = ANTLRFactory.getListener(lenguaje, parser, strings);
 		ParseTreeWalker walker = new ParseTreeWalker();
 		walker.walk(extractor, tree);
-
 		return ANTLRFactory.getMarcas(lenguaje, extractor);
 	}
 
-	public static void fuenteDesmarcado(BufferedReader br, PrintWriter pw, Lenguaje lenguaje) {
-
-		//FIXME: la seleccion de la marca de acuerdo al lenguaje no va aca
-		String gramaticaMarca = (lenguaje == Lenguaje.PYTHON3) ? "# /.*/" : "/\\*/[^/]*/\\*/";
-		br.lines().forEach(linea -> pw.println(linea.replaceAll(gramaticaMarca, "")));
-
-	}
 
 	private static String fuenteDesmarcado(BufferedReader br, Lenguaje lenguaje) {
 		//FIXME: la seleccion de la marca de acuerdo al lenguaje no va aca
 		String gramaticaMarca = (lenguaje == Lenguaje.PYTHON3) ? "# /.*/" : "/\\*/[^/]*/\\*/";
-
-		StringBuilder programaCompleto = new StringBuilder();
-
+		StringBuilder programaCompleto = new StringBuilder()
 		br.lines().forEach(linea -> programaCompleto
-				.append(linea.replaceAll(gramaticaMarca, ""))
+				.append(linea.replaceAll(gramaticaMarca, ""));
 				.append("\n"));
-		return programaCompleto.toString();
+		return programaCompleto.toString()
 
 	}
 
-	private static void algoritmoMarcado(BufferedReader brPreprocesado, PrintWriter pw, List<AnotacionMarca> todasMarcas) throws IOException {
 
-		/*
-		 * Se recorre la lista de marcas
-		 * p/cada marca hay que imprimir en
-		 * la salida las filas hasta que
-		 * la fila coincida con alguna marca
-		 * PARA(CadaMarca)
-		 * MIENTRAS(Fila != FilaMarca) IMPRIMIR(PosicionDentroDeFila, Fila.length())
-		 * SI NO IMPRIMIR(HastaLaMarca),IMPRIMIR(Marca) <- Guardar PosicionDentroDeFila
-		 */
-
-		String entrada;
-		int nroFila = 1;
-
-		entrada = brPreprocesado.readLine();
-		int posEnFila = 0;
-		Collections.sort(todasMarcas);
-		Iterator<AnotacionMarca> it = todasMarcas.iterator();
-		AnotacionMarca marca;
-		AnotacionMarca marcaSiguiente = null;
-
-		while (it.hasNext() || marcaSiguiente != null) {
-			marca = (marcaSiguiente != null) ? marcaSiguiente : it.next();
-			marcaSiguiente = it.hasNext() ? it.next() : null;
-
-			while (nroFila != marca.getFila()) {
-				pw.println(entrada.substring(posEnFila));
-				entrada = brPreprocesado.readLine();
-				nroFila++;
-				posEnFila = 0;
-			}
-			// Coincide la linea, se imprime hasta la marca y luego se imprime la marca.
-			pw.print(entrada.substring(posEnFila, marca.getPosicion() + 1));
-			pw.print(marca.getInicioComentario());
-			pw.print(marca.getMarca());
-
-			// En este punto nroFila coincide con marca.getFila()
-			// Asegurar ademas que posEnFila coincide con marca.getPosicion()
-			posEnFila = marca.getPosicion();
-
-			while (marcaSiguiente != null) {
-				if (nroFila != marcaSiguiente.getFila()
-						|| posEnFila != marcaSiguiente.getPosicion()) break;
-
-				pw.print(" y " + marcaSiguiente.getMarca());
-				marcaSiguiente = it.hasNext() ? it.next() : null;
-			}
-			pw.print(marca.getFinComentario());
-			posEnFila = posEnFila + 1;
-		}
-
-		// Terminaron las marcas, imprimir el resto de la entrada
-		pw.println(entrada.substring(posEnFila));
-		entrada = brPreprocesado.readLine();
-		while(entrada != null) {
-			pw.println(entrada);
-			entrada = brPreprocesado.readLine();
-		}
-	}
-
-	public static void fuenteMarcado(BufferedReader br, PrintWriter pw, Lenguaje programmingLanguage, String language) throws IOException {
-
+	public static List<AnotacionMarca> fuenteMarcado(BufferedReader br, Lenguaje programmingLanguage, String language) {
 		String armaCompleto = fuenteDesmarcado(br, programmingLanguage);
-
 		CharStream antlrEntrada = CharStreams.fromString(armaCompleto);
-		
 		BufferedReader brPreprocesado = new BufferedReader(new StringReader(armaCompleto));
-		
-		List<AnotacionMarca> todasMarcas = iniciaTranslationUnit(antlrEntrada, programmingLanguage, language);
-
-        algoritmoMarcado(brPreprocesado, pw, todasMarcas);
-
+		return iniciaTranslationUnit(antlrEntrada, programmingLanguage, language);
 	}
 
-	public static void reproducirAudio(Integer caso) throws Exception {
-		Clip sonido = AudioSystem.getClip();
-
-		switch (caso) {
-			case 0:
-				sonido.open(AudioSystem.getAudioInputStream(Simae.class.getResource("success.wav")));
-				break;
-			default:
-				sonido.open(AudioSystem.getAudioInputStream(Simae.class.getResource("error.wav")));
-				break;
-		}
-
-		sonido.start();
-		do {
-			Thread.sleep(15);
-		} while (sonido.isRunning());
-		sonido.close();
-
-	}
 }
